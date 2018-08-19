84e4e34022e5528d27cb53aac1947040e21eb5d1
Paul Joannon <hello@pauljoannon.com>
2018-08-19 21:36:34 +0200

    can add, sub and mult points

--- src/point.ts
+++ src/point.ts
@@ -2,9 +2,12 @@ export class Point {
     private x : number;
     private y : number;
 
-    constructor(x : number, y : number) {
-        this.x = x;
-        this.y = y;
+    constructor();
+    constructor(x : number)
+    constructor(x : number, y : number);
+    constructor(x? : number, y? : number){
+        this.x = x || 0;
+        this.y = y || x || 0;
     }
 
     get X() : number {
@@ -21,5 +24,17 @@ export class Point {
 
     set Y(y : number) {
         this.y = y;
+    }
+
+    Add(point : Point) : Point {
+        return new Point(this.X + point.X, this.Y + point.Y);
+    }
+
+    Sub(point : Point) : Point {
+        return this.Add(new Point(-point.X, -point.Y));
+    }
+
+    Mult(point : Point) : Point {
+        return new Point(this.X * point.X, this.Y * point.Y);
     }
 }
