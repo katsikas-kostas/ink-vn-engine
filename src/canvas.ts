00bec4bea91d8e7503fd929ae00aeb714aa8a041
Paul Joannon <hello@pauljoannon.com>
2018-08-19 19:14:53 +0200

    some basic text display and continue mechanics (no choices)

---
+++ src/canvas.ts
@@ -0,0 +1,36 @@
+export class Canvas {
+    element : HTMLCanvasElement;
+    ctx : CanvasRenderingContext2D;
+
+    textColor : string = "black";
+
+    constructor(container_id : string, width : number, height : number) {
+        const container = document.getElementById(container_id);
+
+        if (container.tagName == "canvas") {
+            this.element = <HTMLCanvasElement> container;
+        } else {
+            this.element = document.createElement("canvas");
+            container.appendChild(this.element);
+        }
+
+        this.element.width = width;
+        this.element.height = height;
+
+        this.ctx = this.element.getContext("2d");
+        if (!this.ctx) {
+        }
+
+        this.Clear();
+    }
+
+    Clear() : void {
+        this.ctx.clearRect(0, 0, this.element.width, this.element.height);
+    }
+
+    DrawText(text : string) : void {
+        this.ctx.fillStyle = this.textColor;
+        this.ctx.font = "24px sans-serif";
+        this.ctx.fillText(text, 0, 200, this.element.width);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,90 @@
-import { sayHello } from "./greet";
+import { Canvas } from "./canvas";
 import * as InkJs from "inkjs";
 
-function showHello(divName: string, name: string) {
-    const elt = document.getElementById(divName);
-    elt.innerText = sayHello(name);
+enum State {
+    Waiting,
+    TextAppearing
 }
 
 export class VisualNovInk {
-    constructor(story_filename : string) {
+    story : InkJs.Story;
+    canvas : Canvas;
+
+    state : State;
+
+    currentText : string;
+    textSpeed : number = 20; // In char per second
+    currentAnimationRequest : number;
+    currentTimeout : number;
+
+    constructor(story_filename : string, container_id : string, width : number, height : number) {
+        this.canvas = new Canvas(container_id, width, height);
+
         fetch(story_filename).then((response) => response.text()).then((rawStory) => {
-            let story = new InkJs.Story(rawStory);
-            if (story.canContinue) {
-                story.Continue();
-                showHello("greeting", story.currentText);
-            } else {    
-                showHello("greeting", ":/");
-            }
+            this.story = new InkJs.Story(rawStory);
+            this.continue();
         });
+    }
+
+    continue() : void {
+        if (this.story.canContinue) {
+            this.story.Continue();
+            this.state = State.TextAppearing;
+            this.currentText = "";
+            this.requestStep();
+            this.canvas.element.onclick = this.click.bind(this);
+        } else {
+        }
+    }
+
+    step(timestamp : number) : void {
+        this.currentAnimationRequest = null;
+        this.currentTimeout = null;
+
+        this.canvas.Clear();
+
+        switch (this.state) {
+            case State.TextAppearing: {
+                if (this.currentText.length >= this.story.currentText.length) {
+                    this.state = State.Waiting;
+                } else {
+                    this.currentText += this.story.currentText.slice(this.currentText.length, this.currentText.length + 1);
+                    this.canvas.DrawText(this.currentText);
+                    this.currentTimeout = setTimeout(() => this.requestStep(), 1000 / this.textSpeed);
+                }
+                break;
+            }
+            case State.Waiting: {
+                this.canvas.DrawText(this.currentText);
+                break;
+            }
+        }
+    }
+
+    click() : void {
+        switch (this.state) {
+            case State.TextAppearing: {
+                // Skip apparition
+                if (this.currentAnimationRequest != null) {
+                    window.cancelAnimationFrame(this.currentAnimationRequest);
+                }
+                if (this.currentTimeout != null) {
+                    window.clearTimeout(this.currentTimeout);
+                    this.currentTimeout = null;
+                }
+                this.state = State.Waiting;
+                this.currentText = this.story.currentText;
+                this.requestStep();
+                break;
+            }
+            case State.Waiting: {
+                this.continue();
+                break;
+            }
+        }
+    }
+
+    requestStep() : void {
+        this.currentAnimationRequest = window.requestAnimationFrame(this.step.bind(this));
     }
 }
